58af2fbbb72397aeae4f2a6c157501cdbf172850
snjaypee-arch <sn.jaypee@gmail.com>
2024-03-25 01:41:15 -0300

    docs: Add README

--- Program.cs
+++ Program.cs
@@ -16,9 +16,9 @@ const int chunkSize = 200_000;
 // RabbitMQ.
 ConnectionFactory factory = new()
 {
-    HostName = "localhost",
-    UserName = "rabbit",
-    Password = "mq"
+  HostName = "localhost",
+  UserName = "rabbit",
+  Password = "mq"
 };
 IConnection connection = factory.CreateConnection();
 IModel channel = connection.CreateModel();
@@ -33,88 +33,88 @@ channel.QueueDeclare(
 EventingBasicConsumer consumer = new(channel);
 consumer.Received += async (model, ea) =>
 {
-    // Set data for this concilliation.
-    string concilliationId = Guid.NewGuid().ToString();
-    await using var conn = await dataSource.OpenConnectionAsync();
+  // Set data for this concilliation.
+  string concilliationId = Guid.NewGuid().ToString();
+  await using var conn = await dataSource.OpenConnectionAsync();
 
-    FileHelper fileHelper = new(concilliationId);
+  FileHelper fileHelper = new(concilliationId);
 
-    Console.WriteLine($"[{concilliationId}] Started processing!");
-    try
+  Console.WriteLine($"[{concilliationId}] Started processing!");
+  try
+  {
+    string serialized = Encoding.UTF8.GetString(ea.Body.ToArray());
+    ProcessConcilliationDTO? dto = JsonSerializer.Deserialize<ProcessConcilliationDTO>(serialized);
+    if (dto is null) throw new Exception("Invalid DTO");
+
+
+    // Sorting. (fileToDatabase, statusDiff, ok).
+    using (StreamReader pspFileReader = new StreamReader(dto.File, Encoding.UTF8))
     {
-        string serialized = Encoding.UTF8.GetString(ea.Body.ToArray());
-        ProcessConcilliationDTO? dto = JsonSerializer.Deserialize<ProcessConcilliationDTO>(serialized);
-        if (dto is null) throw new Exception("Invalid DTO");
+      var startTime = DateTime.Now;
+      while (pspFileReader.Peek() > -1)
+      {
+        // Get chunks.
+        List<ConcilliationDTO> pspPayments = await FileStreamReader.StreamReadPaymentChunk(pspFileReader, chunkSize);
+        List<ConcilliationDTO> databasePayments = await DatabaseHandler.FindPayments(conn, pspPayments, dto.PaymentProviderId, dto.Date);
 
+        await PaymentUtils.SortDiffAndMissing(pspPayments, databasePayments, fileHelper);
 
-        // Sorting. (fileToDatabase, statusDiff, ok).
-        using (StreamReader pspFileReader = new StreamReader(dto.File, Encoding.UTF8))
-        {
-            var startTime = DateTime.Now;
-            while (pspFileReader.Peek() > -1)
-            {
-                // Get chunks.
-                List<ConcilliationDTO> pspPayments = await FileStreamReader.StreamReadPaymentChunk(pspFileReader, chunkSize);
-                List<ConcilliationDTO> databasePayments = await DatabaseHandler.FindPayments(conn, pspPayments, dto.PaymentProviderId, dto.Date);
-
-                await PaymentUtils.SortDiffAndMissing(pspPayments, databasePayments, fileHelper);
-
-            }
-            Console.WriteLine($"[{concilliationId}] Sorting finished: FileToDatabase/DifferentStatus! ({(DateTime.Now - startTime).TotalSeconds}sec)");
-        }
-
-        // Sorting. (databaseToFile)
-        using (StreamWriter dbOnly = new(fileHelper.DatabaseToFileFilename, append: true))
-        {
-            var startTime = DateTime.Now;
-            List<ConcilliationDTO> dbPayments = new();
-            int iteration = 0;
-
-            // Iterate over database.
-            do
-            {
-                dbPayments = await DatabaseHandler.QueryPayments(conn, chunkSize, iteration, dto.PaymentProviderId, dto.Date);
-                iteration++;
-
-                await PaymentUtils.SortDatabaseOnly(dbPayments, fileHelper);
-
-            }
-            while (dbPayments.Count > 0);
-
-            Console.WriteLine($"[{concilliationId}] Sorting finished: DatabaseToFile! ({(DateTime.Now - startTime).TotalSeconds}sec)");
-        }
-
-        // Assembly.
-        var startTimeAssemble = DateTime.Now;
-        await PaymentUtils.Assemble(fileHelper);
-        Console.WriteLine($"[{concilliationId}] Assemble Finished! ({(DateTime.Now - startTimeAssemble).TotalSeconds}sec)");
-
-
-        // Envie para a PSP novamente.
-        StreamReader outputFile = new(fileHelper.AssemblyFilename);
-        HttpContent content = new StreamContent(outputFile.BaseStream);
-
-        HttpClient client = new();
-        var response = client.PostAsync(dto.Postback, content);
-
-        Console.WriteLine("Request finished!");
+      }
+      Console.WriteLine($"[{concilliationId}] Sorting finished: FileToDatabase/DifferentStatus! ({(DateTime.Now - startTime).TotalSeconds}sec)");
     }
 
-    catch (Exception e)
+    // Sorting. (databaseToFile)
+    using (StreamWriter dbOnly = new(fileHelper.DatabaseToFileFilename, append: true))
     {
-        Console.WriteLine(e.Message);
-        Console.WriteLine(e.StackTrace);
-        channel.BasicReject(ea.DeliveryTag, requeue: false);
+      var startTime = DateTime.Now;
+      List<ConcilliationDTO> dbPayments = new();
+      int iteration = 0;
 
-        return;
+      // Iterate over database.
+      do
+      {
+        dbPayments = await DatabaseHandler.QueryPayments(conn, chunkSize, iteration, dto.PaymentProviderId, dto.Date);
+        iteration++;
+
+        await PaymentUtils.SortDatabaseOnly(dbPayments, fileHelper);
+
+      }
+      while (dbPayments.Count > 0);
+
+      Console.WriteLine($"[{concilliationId}] Sorting finished: DatabaseToFile! ({(DateTime.Now - startTime).TotalSeconds}sec)");
     }
 
-    // Cleaning.
-    channel.BasicAck(ea.DeliveryTag, multiple: false);
-    await conn.CloseAsync();
-    fileHelper.CleanTempFiles();
+    // Assembly.
+    var startTimeAssemble = DateTime.Now;
+    await PaymentUtils.Assemble(fileHelper);
+    Console.WriteLine($"[{concilliationId}] Assemble Finished! ({(DateTime.Now - startTimeAssemble).TotalSeconds}sec)");
 
-    Console.WriteLine($"[{concilliationId}] Finished processing!");
+
+    // Envie para a PSP novamente.
+    StreamReader outputFile = new(fileHelper.AssemblyFilename);
+    HttpContent content = new StreamContent(outputFile.BaseStream);
+
+    HttpClient client = new();
+    var response = client.PostAsync(dto.Postback, content);
+
+    Console.WriteLine("Request finished!");
+  }
+
+  catch (Exception e)
+  {
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+    channel.BasicReject(ea.DeliveryTag, requeue: false);
+
+    return;
+  }
+
+  // Cleaning.
+  channel.BasicAck(ea.DeliveryTag, multiple: false);
+  await conn.CloseAsync();
+  fileHelper.CleanTempFiles();
+
+  Console.WriteLine($"[{concilliationId}] Finished processing!");
 };
 
 
---
+++ README.md
@@ -0,0 +1,16 @@
+## Configurando o Enviroment
+
+Abra o program.cs e altere (Se necessário) as seguintes variavéis:
+
+- connectionString
+- Connection Factory.
+
+## Como Rodar?
+
+Você pode iniciar o worker com:
+
+```bash
+dotnet run
+```
+
+Caso queira alterar o ChunkSize, abra a program.cs e altere a variável `ChunkSize`.
